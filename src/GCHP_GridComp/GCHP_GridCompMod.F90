24a6efa6be559ab3740e5499f5e4c0469090ae57
Shreya Sharma <shreya22@mit.edu>
2024-08-08 09:37:34 -0400

    Changes to send Fine SS from Chem to MAM

--- src/GCHP_GridComp/GCHP_GridCompMod.F90
+++ src/GCHP_GridComp/GCHP_GridCompMod.F90
@@ -292,6 +292,14 @@ contains
         DST_ID = MAM, SRC_ID = ACHEM, __RC__  )
 #else
 
+! To test direct passing of species in mode from GEOSChem to MAM
+
+CALL MAPL_AddConnectivity (GC, &
+         SRC_NAME = (/'SALA_MAM        '/), &
+         DST_NAME = (/'SS_A_FSS        '/), &
+         DST_ID = MAM, SRC_ID = CHEM, __RC__ )
+
+#if 0
      CALL MAPL_AddConnectivity ( GC, &
         SRC_NAME  = (/'SO2_MAM2        ', 'H2SO4_MAM       ', 'NH3_MAM         ', 'SOA_MAM         ', 'SO4_PRDR_MAM    ', 'NH4_PRDR_MAM    ',   &
                       'SO2_MAM         ', 'SO2_MAM         ', 'SO2_MAM         ', 'SO2_MAM         ', 'SO2_MAM         ', 'SO2_MAM         ',   &     
@@ -304,6 +312,8 @@ contains
                       '_DMS_gas        ', '_MSA_gas        ', '_SO2_gas        ', '_H2SO4_gas      ', '_NH3_gas        ', '_SOA_GAS_gas    ',   &
                       '_DMS_aq         ', '_MSA_aq         ', '_SO2_aq         ', '_H2SO4_aq       ', '_NH3_aq         ', '_SOA_GAS_aq     '/), &
         DST_ID = MAM, SRC_ID = CHEM, __RC__  )
+#endif
+
 #endif
 
      call MAPL_TimerAdd(GC, name="RUN", RC=STATUS)
